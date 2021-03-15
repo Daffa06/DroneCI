ea6eccc59e5b595280e48fd9d65359dbd357521a
Daffa06 <Daffa@DaffaMX>
2021-03-15 20:51:58 +0700

    Edit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$00ebcf294159060185bfdb82a180328d2cb56733@github.com/Tea-Kernel/Tea-Kernel-X00T.git -b CLEAN
+git clone https://$GITHUB_TOKEN@github.com/Tea-Kernel/Tea-Kernel-X00T.git -b CLEAN
 cd Tea-Kernel-X00T
 curl https://raw.githubusercontent.com/Daffa06/myscripts/master/MASTER.sh | bash
