8354a43eb3c1073418953b0a6267942b5efb0855
Daffa06 <Daffa@DaffaMX>
2021-03-18 19:03:45 +0700

    Change

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/Daffa06/Tea_Kernel_X00T.git -b MATE
 cd Tea_Kernel_X00T
-curl https://raw.githubusercontent.com/Daffa06/myscripts/master/GCC102.sh | bash
+curl https://raw.githubusercontent.com/Daffa06/myscripts/master/MASTER.sh | bash
