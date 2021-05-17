f120d227d1d0c833287d18723dd6863a17feec1f
Daffa06 <kumaraprastya@gmail.com>
2021-05-18 06:06:19 +0700

    Back to GCC 10.2

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/Daffa06/Foxi_EAS_X00T.git -b EAS
 cd Foxi_EAS_X00T
-curl https://raw.githubusercontent.com/Daffa06/myscripts/master/CLANG11.sh | bash
+curl https://raw.githubusercontent.com/Daffa06/myscripts/master/GCC102.sh | bash
