40dc045b0d760cac6f6d2cda27933fde7e2f0341
Daffa06 <kumaraprastya@gmail.com>
2021-05-20 02:18:23 +0700

    Build with clang 11

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/Daffa06/Foxi_X00T_EAS.git -b EAS
-cd Foxi_EAS_X00T
+cd Foxi_X00T_EAS
 curl https://raw.githubusercontent.com/Daffa06/myscripts/master/CLANG11.sh | bash
