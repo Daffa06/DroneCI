d526fa7485596bbdb57e9374f887f36090616430
Daffa06 <kumaraprastya@gmail.com>
2021-05-23 20:45:18 +0700

    Build with GCC 10.2

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/Daffa06/Foxi_X00T_EAS.git -b EAS
+git clone https://$GITHUB_TOKEN@github.com/Daffa06/Foxi_X00T_EAS.git -b EAS_4.4.r42
 cd Foxi_X00T_EAS
 curl https://raw.githubusercontent.com/Daffa06/myscripts/master/GCC102.sh | bash
